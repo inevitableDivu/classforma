4349cda6d58fde701f0bab92d920804694a9edc0
Divyansh Pandey <aaravdivyansh@gmail.com>
2021-10-11 19:34:48 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './css/style.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ThemeContext from './context/ThemeContext'
+import AnnotateContext from './context/AnnotateContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext>
+      <AnnotateContext>
+        <App />
+      </AnnotateContext>
+    </ThemeContext>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
